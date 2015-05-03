ca0bf165ae3a4a6e1026ef2fdc721d19f93bd9b7
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-05-03 09:52:13 +0900

    define what to implement

--- lib/predef.rb
+++ lib/predef.rb
@@ -1,3 +1,13 @@
 module Predef
   VERSION = "0.0.1"
+
+  def self.predef klass, method_name, &definition
+  end
+
+  def self.unpredef klass, method_name = nil
+  end
+
+  class PredefWrapper < ::Module
+  end
+
 end
