bb4bdfdb055ecf5abd99fbc821632959d8d7fe90
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-05-21 20:29:56 +0900

    add simplest test case

--- test/test.rb
+++ test/test.rb
@@ -1,1 +1,21 @@
 require_relative 'test_helper'
+
+class Hoge
+  def foo arg
+    'bar ' + arg
+  end
+end
+
+example 'Predef overrides methods of a class' do
+  Predef.predef Hoge, :foo do|arg|
+    super(arg) + ' baz'
+  end
+
+  actual = Hoge.new.foo('arg')
+  expected = 'bar arg baz'
+  test(
+    'overridden method can use super.',
+    (actual == expected),
+    "Expected #{actual.inspect} to equal to #{expected.inspect}."
+  )
+end
