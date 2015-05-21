d9083b357be1ddf0e8e6a99be9ba4abdee1fd850
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-05-21 21:22:42 +0900

    refactor: less nesting

--- lib/predef.rb
+++ lib/predef.rb
@@ -22,16 +22,14 @@ class Predef < Module
   end
 
   def self.unpredef klass, method_name
-    if defined? klass.__PREDEF__
-      klass.__PREDEF__.module_eval do
-        if self.public_method_defined? method_name
-          remove_method method_name
-        else
-          raise ::Predef::Error, "#{klass}##{method_name} has never been predef-ed!"
-        end
+    raise ::Predef::Error, "#{klass} has never been predef-ed!" unless defined? klass.__PREDEF__
+
+    klass.__PREDEF__.module_eval do
+      unless self.public_method_defined? method_name
+        raise ::Predef::Error, "#{klass}##{method_name} has never been predef-ed!"
       end
-    else
-      raise ::Predef::Error, "#{klass} has never been predef-ed!"
+
+      remove_method method_name
     end
   end
 
