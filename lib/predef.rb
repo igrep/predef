53ad8e607f1d053735c7b8e5fcc509334942b01f
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-05-21 20:47:08 +0900

    implemnt to all of the test so far

--- lib/predef.rb
+++ lib/predef.rb
@@ -1,13 +1,34 @@
-module Predef
+class Predef < Module
   VERSION = "0.0.1"
 
+  private_class_method :new
+
   def self.predef klass, method_name, &definition
+    if defined? klass.__PREDEF__
+      instance = klass.__PREDEF__
+    else
+      instance = new(klass)
+
+      singleton_class = class << klass; self ; end
+      singleton_class.class_eval do
+        define_method(:__PREDEF__) { instance }
+      end
+    end
+
+    instance.module_eval do
+      define_method(method_name, &definition)
+    end
+    method_name
   end
 
   def self.unpredef klass, method_name = nil
   end
 
-  class PredefWrapper < ::Module
+  def initialize klass
+    prepend_features klass
+  end
+
+  module Refinements
   end
 
 end
