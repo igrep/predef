1ecc40cb25dacd47348d7c1bf43ca2d47b5a4e92
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-05-25 20:50:20 -0600

    add unpredef! method
    I'm going to use this for crispy internally

--- lib/predef.rb
+++ lib/predef.rb
@@ -32,11 +32,14 @@ class Predef < Module
   def self.unpredef klass, method_name
     raise ::Predef::Error, "#{klass} has never been predef-ed!" unless defined? klass.__PREDEF__
 
-    klass.__PREDEF__.module_eval do
-      unless self.public_method_defined? method_name
-        raise ::Predef::Error, "#{klass}##{method_name} has never been predef-ed!"
-      end
+    unless klass.__PREDEF__.public_method_defined? method_name
+      raise ::Predef::Error, "#{klass}##{method_name} has never been predef-ed!"
+    end
+    self.unpredef! klass, method_name
+  end
 
+  def self.unpredef! klass, method_name
+    klass.__PREDEF__.module_eval do
       remove_method method_name
     end
   end
