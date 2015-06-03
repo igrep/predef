9ed21e78c10ed3bc3621f4cee689a47d391363f0
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2015-06-03 22:58:06 +0900

    give up to measure coverage

--- Gemfile
+++ Gemfile
@@ -2,5 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in predef.gemspec
 gemspec
-
-gem 'codeclimate-test-reporter', require: nil if ENV['CODECLIMATE_REPO_TOKEN']
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,3 @@
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
-
 def example description, &block
   puts "- #{description}"
   block.call
